cd92d1e1796ded35f60c9f05e57b07a11d89a08a
Shadrac Reyes <ShadReyes@MacBook-Pro.local>
2022-04-14 23:35:49 -0600

    Some fun config stuff

--- src/Screens/authorizationScreens/Login.tsx
+++ src/Screens/authorizationScreens/Login.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState } from "react";
 
 export default function Login() {
@@ -76,15 +77,16 @@ export default function Login() {
                   </div>
                 </div>
                 <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
-                  <div className="text-white px-3 py-4 p-md-5 mx-md-4">
+                  {/* <div className="text-white px-3 py-4 p-md-5 mx-md-4">
                     <h4 className="mb-4">We are more than just a company</h4>
-                    <p className="small mb-0">
+                    <p style={{ color: "black" }} className="small mb-0">
                       Lorem ipsum dolor sit amet, consectetur adipisicing elit,
                       sed do eiusmod tempor incididunt ut labore et dolore magna
                       aliqua. Ut enim ad minim veniam, quis nostrud exercitation
                       ullamco laboris nisi ut aliquip ex ea commodo consequat.
                     </p>
-                  </div>
+                  </div> */}
+                  <img src={require("./../../images")} />
                 </div>
               </div>
             </div>
