6ec040e60947f8d1e634b2cb9e40fb06f3bb5b51
Shadrac Reyes <ShadReyes@MacBook-Pro.local>
2022-04-14 23:14:35 -0600

    Finally got this working

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Login from "./Screens/authorizationScreens/index.tsx";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-function App() {
-  return <Login />;
-}
-
-export default App;
